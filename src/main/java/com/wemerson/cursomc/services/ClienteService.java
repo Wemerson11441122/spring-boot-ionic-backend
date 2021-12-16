35554009d9df5d61cab9f111d1604c528a605c78
Wemerson Borjes <wemersonemao@gmail.com>
2021-12-16 10:16:01 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/wemerson/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.wemerson.cursomc.services;
+
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wemerson.cursomc.domain.Cliente;
+import com.wemerson.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName(), null));
+	}
+
+}
