8a0c824349c1e520f7a5044a414de2f2a80dc7b7
Wemerson Borjes <wemersonemao@gmail.com>
2022-02-03 09:44:34 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/wemerson/cursomc/services/CategoriaService.java
+++ src/main/java/com/wemerson/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.wemerson.cursomc.domain.Categoria;
+import com.wemerson.cursomc.dto.CategoriaDTO;
 import com.wemerson.cursomc.repositories.CategoriaRepository;
 import com.wemerson.cursomc.services.exceptions.DataIntegrityException;
 import com.wemerson.cursomc.services.exceptions.ObjectNotFoundExceptions;
@@ -59,4 +60,11 @@ public class CategoriaService {
 	   return repo.findAll(pageRequest);
    
 }
+   
+   public Categoria fromDTO(CategoriaDTO objDto) {
+	   return new Categoria(objDto.getId(), objDto.getNome());
+   }
+   
+   
+   
 }
